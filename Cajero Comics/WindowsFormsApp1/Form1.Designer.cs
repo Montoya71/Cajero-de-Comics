0f755b1f7753be9063d2b989216b90e946f9d93a
Montoya <75280351+Montoya71@users.noreply.github.com>
2020-11-30 17:58:35 -0600

    Add files via upload

---
+++ Cajero Comics/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblBienvenidos = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.lblRecibido = new System.Windows.Forms.Label();
+            this.txtRecibido = new System.Windows.Forms.TextBox();
+            this.lblCambio = new System.Windows.Forms.Label();
+            this.txtCambio = new System.Windows.Forms.TextBox();
+            this.btnMultiplicacion = new System.Windows.Forms.Button();
+            this.btnResta = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblBienvenidos
+            // 
+            this.lblBienvenidos.AutoSize = true;
+            this.lblBienvenidos.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblBienvenidos.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBienvenidos.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblBienvenidos.Location = new System.Drawing.Point(30, 20);
+            this.lblBienvenidos.Name = "lblBienvenidos";
+            this.lblBienvenidos.Size = new System.Drawing.Size(175, 33);
+            this.lblBienvenidos.TabIndex = 0;
+            this.lblBienvenidos.Text = "Bienvenidos";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecio.Location = new System.Drawing.Point(26, 96);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(57, 20);
+            this.lblPrecio.TabIndex = 1;
+            this.lblPrecio.Text = "Precio:";
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCantidad.Location = new System.Drawing.Point(195, 96);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(77, 20);
+            this.lblCantidad.TabIndex = 2;
+            this.lblCantidad.Text = "Cantidad:";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(26, 148);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(48, 20);
+            this.lblTotal.TabIndex = 3;
+            this.lblTotal.Text = "Total:";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(89, 96);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 4;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(276, 96);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidad.TabIndex = 5;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(80, 150);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtTotal.TabIndex = 6;
+            // 
+            // lblRecibido
+            // 
+            this.lblRecibido.AutoSize = true;
+            this.lblRecibido.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecibido.Location = new System.Drawing.Point(26, 194);
+            this.lblRecibido.Name = "lblRecibido";
+            this.lblRecibido.Size = new System.Drawing.Size(136, 20);
+            this.lblRecibido.TabIndex = 7;
+            this.lblRecibido.Text = "Cantidad Pagada:";
+            // 
+            // txtRecibido
+            // 
+            this.txtRecibido.Location = new System.Drawing.Point(168, 196);
+            this.txtRecibido.Name = "txtRecibido";
+            this.txtRecibido.Size = new System.Drawing.Size(100, 20);
+            this.txtRecibido.TabIndex = 8;
+            // 
+            // lblCambio
+            // 
+            this.lblCambio.AutoSize = true;
+            this.lblCambio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCambio.Location = new System.Drawing.Point(379, 274);
+            this.lblCambio.Name = "lblCambio";
+            this.lblCambio.Size = new System.Drawing.Size(111, 20);
+            this.lblCambio.TabIndex = 9;
+            this.lblCambio.Text = " Tu cambio es:";
+            // 
+            // txtCambio
+            // 
+            this.txtCambio.Location = new System.Drawing.Point(499, 274);
+            this.txtCambio.Name = "txtCambio";
+            this.txtCambio.Size = new System.Drawing.Size(92, 20);
+            this.txtCambio.TabIndex = 10;
+            // 
+            // btnMultiplicacion
+            // 
+            this.btnMultiplicacion.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnMultiplicacion.Location = new System.Drawing.Point(383, 94);
+            this.btnMultiplicacion.Name = "btnMultiplicacion";
+            this.btnMultiplicacion.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiplicacion.TabIndex = 11;
+            this.btnMultiplicacion.Text = "Confirmar";
+            this.btnMultiplicacion.UseVisualStyleBackColor = true;
+            this.btnMultiplicacion.Click += new System.EventHandler(this.btnMultiplicacion_Click);
+            // 
+            // btnResta
+            // 
+            this.btnResta.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnResta.Location = new System.Drawing.Point(276, 194);
+            this.btnResta.Name = "btnResta";
+            this.btnResta.Size = new System.Drawing.Size(75, 23);
+            this.btnResta.TabIndex = 12;
+            this.btnResta.Text = "Confirmar";
+            this.btnResta.UseVisualStyleBackColor = true;
+            this.btnResta.Click += new System.EventHandler(this.btnResta_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(503, 297);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Con IVA incluido ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(211, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(221, 70);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(675, 368);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnResta);
+            this.Controls.Add(this.btnMultiplicacion);
+            this.Controls.Add(this.txtCambio);
+            this.Controls.Add(this.lblCambio);
+            this.Controls.Add(this.txtRecibido);
+            this.Controls.Add(this.lblRecibido);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.lblBienvenidos);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBienvenidos;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label lblRecibido;
+        private System.Windows.Forms.TextBox txtRecibido;
+        private System.Windows.Forms.Label lblCambio;
+        private System.Windows.Forms.TextBox txtCambio;
+        private System.Windows.Forms.Button btnMultiplicacion;
+        private System.Windows.Forms.Button btnResta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Cajero Comics/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+         public void btnMultiplicacion_Click(object sender, EventArgs e)
+        {
+            string textoN1;
+            string textoN2;
+            int N1;
+            int N2;
+            int Resultado;
+
+            textoN1 = txtPrecio.Text;
+            textoN2 = txtCantidad.Text;
+            N1 = Convert.ToInt32(textoN1);
+            N2 = Convert.ToInt32(textoN2);
+            Resultado = N1 * N2;
+
+            txtTotal.Text = Resultado.ToString();
+        }
+
+        public void btnResta_Click(object sender, EventArgs e)
+        {
+            string textoN3;
+            string textoN4;
+            float N3;
+            float N4;
+            float Resultado2;
+            float Resultado3;
+            float Resultado4;
+            double V = 0.16;
+
+            textoN3 = txtTotal.Text;
+            textoN4 = txtRecibido.Text;
+            N3 = Convert.ToInt32(txtTotal.Text);
+            N4 = Convert.ToInt32(txtRecibido.Text);
+
+            Resultado2 = N4 - N3;
+            Resultado3 = (float)(Resultado2 * V);
+            Resultado4 = Resultado2 - Resultado3;
+
+            txtCambio.Text = Resultado4.ToString();
+        }
+
+           
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
